c8fc009fb06f674f502296983ff3511aba8094d9
Dan Schultz <dan.schultz@vertexvis.com>
2020-04-22 00:36:21 -0500

    Add picking example

--- examples/helpers.js
+++ examples/helpers.js
@@ -10,9 +10,21 @@ export async function configureViewer(viewer) {
   }
 }
 
+export async function loadDefaultModel(viewer) {
+  const urn = await fetchDefaultModel()
+  const newScene = await viewer.newScene();
+  await newScene.from(urn).execute().then(scene => viewer.load(scene))
+}
+
 async function fetchToken() {
   const resp = await fetch('http://localhost:3000/token');
   return await resp.json();
+}
+
+async function fetchDefaultModel() {
+  const resp = await fetch('http://localhost:3000/model');
+  const json = await resp.json();
+  return json.urn;
 }
 
 function setCredentials(viewer, { clientId, token }) {
---
+++ examples/picking/main.js
@@ -0,0 +1,26 @@
+import { configureViewer, loadDefaultModel } from '../helpers.js';
+
+document.addEventListener('DOMContentLoaded', () => {
+  main();
+});
+
+async function main() {
+  const viewer = document.querySelector('vertex-viewer');
+  await configureViewer(viewer);
+  await loadDefaultModel(viewer)
+
+  viewer.addEventListener('tap', async (event) => {
+    const {position} = event.detail
+    const scene = await viewer.scene();
+    const raycaster = await scene.raycaster();
+
+    const { bomItems } = await raycaster.intersectItems(position).execute();
+
+    if (bomItems.length == 0) {
+      scene.clearAllHighlights().execute()
+    } else {
+      const [item] = bomItems;
+      scene.clearAllHighlights().highlight('#ff0000', s => s.withItemId(item.id)).execute();
+    }
+  });
+}
