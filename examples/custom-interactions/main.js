96bc1a1cb227dddad4efefb2db15ad52d7157b1a
Jeff Murray <jeff.murray@vertexvis.com>
2020-04-27 11:14:09 -0500

    Add double tap handler

--- examples/custom-interactions/main.js
+++ examples/custom-interactions/main.js
@@ -3,13 +3,33 @@ import { configureViewer, loadDefaultModel } from '../helpers.js';
 class CustomInteractionHandler {
   dispose() {
     this.element.removeEventListener('dblclick', this.handleDoubleClick);
+    this.element.removeEventListener('touchstart', this.handleTouchStart);
   }
 
   initialize(element, api) {
     this.api = api;
     this.element = element;
     this.element.addEventListener('dblclick', this.handleDoubleClick);
+    this.element.addEventListener('touchstart', this.handleTouchStart);
   }
+
+  handleTouchStart = async (event) => {
+    if (event.touches.length === 1) {
+      this.tapTimer =
+        this.tapTimer == null
+          ? setTimeout(() => {
+              this.tapTimer = null;
+            }, 500)
+          : null;
+
+      if (this.tapTimer == null) {
+        const viewer = document.querySelector('vertex-viewer');
+        const scene = await viewer.scene();
+
+        scene.camera().viewAll().execute();
+      }
+    }
+  };
 
   handleDoubleClick = async () => {
     const viewer = document.querySelector('vertex-viewer');
@@ -30,4 +50,3 @@ async function main() {
 
   viewer.registerInteractionHandler(new CustomInteractionHandler());
 }
-
