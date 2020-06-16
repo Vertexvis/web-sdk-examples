752156b1ad1324aa8b5976e986bdb9a1b629b57a
Madison Ehlers <49169871+MadisonEhlers-Vertex@users.noreply.github.com>
2020-06-16 16:06:46 -0500

    [SDK-656] Fly to Part example (#4)
    * [SDK-656] Fly to Part example
    
    * removing dev configs
    
    * some cleanup
    
    * fly to part
    
    * using execute

---
+++ examples/fly-to-part/main.js
@@ -0,0 +1,35 @@
+import { configureViewer, loadDefaultModel } from '../helpers.js';
+
+document.addEventListener('DOMContentLoaded', () => {
+  main();
+});
+
+async function main() {
+  const viewer = document.querySelector('vertex-viewer');
+
+  viewer.config = {
+    network: {
+      renderingHost: 'wss://rendering.dev.vertexvis.io',
+      apiHost: 'https://api.dev.vertexvis.io',
+    },
+  };
+
+  await configureViewer(viewer);
+  await loadDefaultModel(viewer);
+
+  viewer.addEventListener('tap', async (event) => {
+    const { position } = event.detail;
+    const scene = await viewer.scene();
+    const raycaster = await scene.raycaster();
+
+    const { bomItems } = await raycaster.intersectItems(position).execute();
+
+    if (bomItems.length == 0) {
+      scene.clearAllHighlights().execute();
+    } else {
+      const [item] = bomItems;
+
+      await scene.camera().flyToPart(item.id).execute();
+    }
+  });
+}
